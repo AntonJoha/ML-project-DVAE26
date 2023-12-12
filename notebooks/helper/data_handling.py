177451f30203c91a72bcaf26ea0b9a2d1eeb50fe
Anton Johansson <antojohansson1@gmail.com>
2023-12-12 10:28:25 +0100

    Updated gitignore

---
+++ notebooks/helper/data_handling.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+
+def data_from_file(file: str):
+    df = pd.read_csv(file)
+    df = df.drop("id", axis=1)
+    df.replace("Unknown", np.NaN)
+    return df
+
+if __name__ == "__main__":
+    print(data_from_file("../data/stroke_raw.csv").head())
